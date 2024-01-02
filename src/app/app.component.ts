0bec15fedde32f9704e11327577acdb8897e7dff
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-01-02 19:34:48 -0300

    update projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, HomeComponent, QuizzComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
